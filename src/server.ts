74d5380b59abf44bf66e301a5f3f13284d5a650a
Alif-Sarker <sarker202454@gmil.com>
2025-12-01 18:55:08 +0600

    todos crud added

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import { Result } from './../node_modules/@types/pg/index.d';
 import { Message } from './../node_modules/esbuild/lib/main.d';
 import express,{Request,Response} from "express";
 import {Pool} from "pg";
@@ -50,6 +51,9 @@ const initDB = async() =>{
 } 
 initDB();
 
+// logger middleware 
+
+
 
 app.get('/', (req:Request, res:Response) => {
   res.send('Hello Alif !')
@@ -84,6 +88,28 @@ app.post('/users', async (req:Request, res:Response) => {
    
 })
 
+
+  
+// Get all user 
+app.get("/users",async (req:Request,res:Response)=>{
+       try{
+           const result = await  pool.query(`SELECT * FROM users`);
+
+           res.status(200).json({
+            success:true,
+            message:"Users retrieved successfully",
+            data:result.rows,
+           })
+
+       }catch(err:any){
+            res.status(500).json({
+            success:false,
+            message:err.message,
+            details:err,
+            })
+       }
+})
+
 // get single  user 
    app.get('/users/:id',async(req:Request, res:Response) =>{
     //    console.log(req.params.id)
@@ -113,15 +139,96 @@ app.post('/users', async (req:Request, res:Response) => {
      
 
    })
-  
-// Get all user 
-app.get("/users",async (req:Request,res:Response)=>{
+
+// Update user 
+  app.put('/users/:id',async(req:Request, res:Response) =>{
+     const {name,email} = req.body;
+     try{
+        const result = await pool.query(`UPDATE users SET name=$1, email=$2 WHERE id=$3 RETURNING * `,[name,email,req.params.id]);
+
+        if(result.rows.length === 0 ){
+            res.status(404).json({
+                success:false,
+                message:"User not found"
+            })
+        }
+        else{
+            res.status(200).json({
+                success:true,
+                message:"User updated successfully",
+                data:result.rows[0],
+            })
+        }
+
+     }catch(err:any){
+         res.status(500).json({
+            success:false,
+            message: err.message,
+        })  
+     }
+     
+
+   })
+
+   // delete 
+    app.delete('/users/:id',async(req:Request, res:Response) =>{ 
+    //    console.log(req.params.id)
+     try{
+        const result = await pool.query(`DELETE FROM users WHERE id = $1`,[req.params.id]);
+
+        if(result.rowCount === 0 ){
+            res.status(404).json({
+                success:false,
+                message:"User not found"
+            })
+        }
+        else{
+            res.status(200).json({
+                success:true,
+                message:"User delete successfully",
+                data:result.rows,
+            })
+        }
+
+     }catch(err:any){
+         res.status(500).json({
+            success:false,
+            message: err.message,
+        })  
+     }
+     
+
+   })
+
+
+   //* Todos CRUD
+   app.post("/todos",async(req:Request, res:Response)=>{
+         const {user_id,title} = req.body;
+         try{
+             const result = await pool.query(`INSERT INTO todos(user_id,title) VALUES($1,$2) RETURNING *`, [user_id,title])
+
+             res.status(201).json({
+                success: true,
+                message: "Todo created",
+                data: result.rows[0]
+             })
+         }catch(err){
+            res.status(500).json({
+                success : false,
+                message: "err.message"
+            })
+
+         }
+   })
+
+   // get all todos
+   app.get("/todos",async (req:Request,res:Response)=>{
        try{
-           const result = await  pool.query(`SELECT * FROM users`);
+           const result = await  pool.query(`SELECT * FROM todos`);
 
            res.status(200).json({
             success:true,
-            message:"Users retrieved successfully",
+            message:"todos retrieved successfully",
             data:result.rows,
            })
 
@@ -132,6 +239,16 @@ app.get("/users",async (req:Request,res:Response)=>{
             details:err,
             })
        }
+})
+
+
+// Default route 
+app.use((req,res)=>{
+    res.status(404).json({
+        success: false,
+        message:"Route not found",
+        path:req.path
+    })
 })
 
 
